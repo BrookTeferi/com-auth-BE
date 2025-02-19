b34e51aa6317879ee58ff31b634e811228a17d8e
BrookTeferi <brookteferi1q2w@gmail.com>
2025-02-19 18:24:23 +0300

    Add initial migrations for Custom_User and Roles models; update URLs to include permissions

--- humsanity/urls.py
+++ humsanity/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework import routers, permissions
 from account_users.views import CustomUserViewSet
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
